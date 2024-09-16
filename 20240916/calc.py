a3a716d489c9b531ababbd11c13aca22900e8bf1
sahithya <sahithyabadri23@gmail.com>
2024-09-16 16:17:50 +0530

    Intro to py

---
+++ 20240916/calc.py
@@ -0,0 +1,12 @@
+def calc(first:int, second:int)->int:
+    sum=first+second
+    diff= first - second
+    product = first * second
+    quotient = first//second
+    return sum,diff,product,quotient
+s,d,p,q = calc(20,6) 
+print(s,d,p,q)
+
+t1 = calc(10,6)
+print(t1)
+print(type(t1))
---
+++ fact.py
@@ -0,0 +1,9 @@
+def find_fact(N):
+    fact = 1
+    for I in range(N,1,-1):
+        fact = fact * I 
+    return fact
+f1 = find_fact(5) 
+f2 = find_fact(4) 
+print(f1)
+print(f2)   
